fb60427d7586217d196d7380649afbe22a2d4714
anggunsdewi <anggunsasmithad@gmail.com>
2024-09-18 16:56:44 +0700

    fixing bugs in tutorial 3

--- main/models.py
+++ main/models.py
@@ -3,12 +3,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class MoodEntry(models.Model):
-    users = models.ForeignKey(User, on_delete=models.CASCADE),
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # tambahkan baris ini
     mood = models.CharField(max_length=255)
     time = models.DateField(auto_now_add=True)
     feelings = models.TextField()
     mood_intensity = models.IntegerField()
+    users = models.ForeignKey(User, on_delete=models.CASCADE)
+    #test
 
     @property
     def is_mood_strong(self):
--- main/views.py
+++ main/views.py
@@ -6,7 +6,8 @@ from django.core import serializers
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 import datetime
 from django.http import HttpResponseRedirect
