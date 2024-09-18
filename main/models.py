c7e9b9bbee8b7c11791f035700de8415bdae84ef
anggunsdewi <anggunsasmithad@gmail.com>
2024-09-18 17:55:13 +0700

    integrate pws with github

--- main/models.py
+++ main/models.py
@@ -3,8 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class MoodEntry(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # tambahkan baris ini
-    mood = models.CharField(max_length=255)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False) 
     time = models.DateField(auto_now_add=True)
     feelings = models.TextField()
     mood_intensity = models.IntegerField()
